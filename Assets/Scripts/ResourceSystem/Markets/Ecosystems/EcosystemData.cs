4c1098b878b32bcbfa159b9685e5e4d66368e43b
leodar163 <leobgely@gmail.com>
2023-11-20 15:37:33 +0100

    start bad decision about ecosystem

---
+++ Assets/Scripts/ResourceSystem/Markets/Ecosystems/EcosystemData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TerrainSystem;
+
+namespace ResourceSystem.Markets.Ecosystems
+{
+    [Serializable]
+    public struct EcosystemData
+    {
+        public TerrainType terrain;
+    }
+}
---
+++ Assets/Scripts/ResourceSystem/Markets/Ecosystems/MotherNature.cs
@@ -0,0 +1,41 @@
+﻿using TerrainSystem;
+using TimeSystem;
+using UnityEngine;
+using Utils;
+using Random = UnityEngine.Random;
+
+namespace ResourceSystem.Markets.Ecosystems
+{
+    public class MotherNature : Singleton<MotherNature>
+    {
+        [SerializeField] private EcosystemData[] _ecosystemsData;
+
+        private void OnEnable()
+        {
+            TimeManager.onMonthEnds += MakeADecision;
+        }
+
+        private void OnDisable()
+        {
+            TimeManager.onMonthEnds -= MakeADecision;
+        }
+
+        private void MakeADecision()
+        {
+            Market randomEcosystem =
+                MarketManager.ecosystemMarkets[Random.Range(0, MarketManager.ecosystemMarkets.Count)];
+
+            TerrainType ecosystemType = randomEcosystem.cells[0].terrain;
+        }
+
+        private static EcosystemData GetEcosystemDataFromTerrain(TerrainType terrain)
+        {
+            foreach (var data in Instance._ecosystemsData)
+            {
+                if (data.terrain == terrain) return data;
+            }
+
+            return default;
+        }
+    }
+}
