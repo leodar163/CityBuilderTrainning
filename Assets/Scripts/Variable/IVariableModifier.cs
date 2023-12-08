57af3b16766d57a30c2fb3c20826a5d7d80fffd3
leodar163 <leobgely@gmail.com>
2023-12-08 18:38:49 +0100

    add subscribable variables

---
+++ Assets/Scripts/Variable/IVariableModifier.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Variable
+{
+    public interface IVariableModifier
+    {
+        public IVariableModifier VariableModifierSelf { get; }
+        
+        public float Add { get; }
+        public float Mult { get; }
+        public float Percent { get; }
+
+        protected List<SubscribableFloat> variables { get; }
+
+        public void ApplyTo(SubscribableFloat variable)
+        {
+            if (variables.Contains(variable)) return;
+            
+            variables.Add(variable);
+            variable.AddModifier(this);
+            
+        }
+
+        public void RemoveFrom(SubscribableFloat variable)
+        {
+            if (!variables.Remove(variable)) return;
+            
+            variable.RemoveModifier(this);
+        }
+
+        public void RemoveFromAllVariable()
+        {
+            while (variables.Count > 0)
+            {
+                RemoveFrom(variables[0]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Variable/SubscribableFloat.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Localization;
+using UnityEngine;
+
+namespace Variable
+{
+    [Serializable]
+    public class SubscribableFloat
+    {
+        [SerializeField] private Sprite _variableIcon;
+        public Sprite VariableIcon => _variableIcon;
+        
+        [SerializeField] private AutoLocalizedString _variableName;
+        public AutoLocalizedString VariableName
+        {
+            get
+            {
+                if (!_changePipeInit)
+                {
+                    _variableName.onStringChanged += _ => onVariableChanged?.Invoke(this);
+                    _changePipeInit = true;
+                }
+                return _variableName;
+            }
+        }
+        private bool _changePipeInit; 
+        
+        private float _baseValue;
+        
+        public float BaseValue
+        {
+            get => _baseValue;
+            set
+            {
+                _baseValue = value;
+                onVariableChanged?.Invoke(this);
+            }
+        }
+        
+        public float Value => GetValue();
+
+        private float _percent;
+        private float _mult;
+        private float _add;
+
+        public readonly List<IVariableModifier> modifiers = new();
+
+        public event Action<SubscribableFloat> onVariableChanged;
+        
+        public void AddModifier(IVariableModifier modifier)
+        {
+            if (modifiers.Contains(modifier)) return;
+            modifiers.Add(modifier);
+
+            _add += modifier.Add;
+            _mult += modifier.Mult;
+            _percent += modifier.Percent;
+
+            onVariableChanged?.Invoke(this);
+        }
+
+        public void RemoveModifier(IVariableModifier modifier)
+        {
+            if (!modifiers.Remove(modifier)) return;
+
+            _add -= modifier.Add;
+            _mult -= modifier.Mult;
+            _percent -= modifier.Percent;
+            
+            onVariableChanged?.Invoke(this);
+        }
+        
+        public float GetValue()
+        {
+            float value = _baseValue;
+            value += _add;
+            value += value * _percent / 100;
+            value *= _mult;
+            return value;
+        }
+    }
+}
