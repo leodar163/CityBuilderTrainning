cfce9454d99c5ab8ce7d9e2cf09322dfde8f3236
leodar163 <leobgely@gmail.com>
2023-12-08 18:04:45 +0100

    add localization automatic variable

---
+++ Assets/Scripts/Localization/AutoLocalizedString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+namespace Localization
+{
+    [Serializable]
+    public class AutoLocalizedString
+    {
+        public static readonly List<AutoLocalizedString> allAutoLocalizedStrings = new();
+
+        [SerializeField] private LocalizedString _localizedString;
+        private string _string = "no_string";
+
+        public string String => _string;
+
+        public event Action<AutoLocalizedString> onStringChanged;
+
+        public AutoLocalizedString()
+        {
+            allAutoLocalizedStrings.Add(this);
+        }
+        
+        public void Init()
+        {
+            if (_localizedString is { IsEmpty: false }) 
+                _string = _localizedString.GetLocalizedString();
+            
+            LocalizationSettings.SelectedLocaleChanged += UpdateString;
+        }
+        
+        private void UpdateString(Locale locale = null)
+        {
+            _string = _localizedString.GetLocalizedString();
+            onStringChanged?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,15 @@
+﻿using Utils;
+
+namespace Localization
+{
+    public class LocalizationManager : Singleton<LocalizationManager>
+    {
+        private void Awake()
+        {
+            foreach (var autoLocalizedString in AutoLocalizedString.allAutoLocalizedStrings)
+            {
+                autoLocalizedString.Init();
+            }
+        }
+    }
+}
